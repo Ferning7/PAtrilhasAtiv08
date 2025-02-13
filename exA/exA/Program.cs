7b387fdcbecb808ed650dec6660c82427a4f5d4d
Matheus <81193702+Ferning7@users.noreply.github.com>
2025-02-13 11:19:20 -0300

    Lista 10 - Método

---
+++ exA/exA/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace exA
+{
+	class Program
+	{
+		static double Quadrado(int n){
+			return Math.Sqrt(n);
+		}
+		
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Digite um número para calcular raiz quadrada");
+			int num = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine("a Raiz quadrada de {0} será {1}", num, Quadrado(num));
+			Console.WriteLine(Math.Sqrt(4));
+			
+			Console.ReadKey(true);
+		}
+		
+		
+		
+	}
+}
---
+++ exB/exB/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace exA
+{
+	class Program
+	{
+		
+		static int NumCaracteres(string word){
+			return word.Length;
+		}
+		
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Digite uma palavra:");
+			string palavra = Console.ReadLine(); 
+			
+			Console.WriteLine("A palavra '{0}' contém {1} letras", palavra, NumCaracteres(palavra));
+			
+			
+			
+			Console.ReadKey(true);
+			
+			
+		}
+		
+		
+		
+	}
+}
