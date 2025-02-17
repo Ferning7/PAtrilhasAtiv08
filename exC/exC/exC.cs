ce8cf097333fde289fef141d5fad5e710b302212
Matheus <81193702+Ferning7@users.noreply.github.com>
2025-02-17 10:07:26 -0300

    Lista 10 - Métodos

---
+++ exC/exC/exC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exC
+{
+    class Program
+    {
+        static int somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números: ");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"A soma de {num1} + {num2}: {somar(num1, num2)}");
+        }
+    }
+}
+
+
+
+
---
+++ exD/exD/exD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exD
+{
+    class exD
+    {
+        static int ListaMaiorNumero(List<int> ListaNumeros)
+        {
+            int maiorNumero = ListaNumeros[0];
+            
+            foreach (int numero in ListaNumeros)
+            {
+                if (numero > ListaNumeros.Max())
+                {
+                    maiorNumero = numero;
+                }
+            }
+            return maiorNumero;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números inteiros");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            
+            
+            List<int> ListaNumeros = new List<int>{num1, num2, 5, 10, 15, 20, 25, 30, 35, 45};
+            int ListaMaiorNumero = ListaNumeros.Max();
+
+            Console.WriteLine($"O maior número da lista é: {ListaMaiorNumero}");
+        }
+    }
+}
+
