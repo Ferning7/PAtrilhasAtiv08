39b3749bc72228376af8addffe03f55962fc10f4
Matheus <81193702+Ferning7@users.noreply.github.com>
2025-02-17 11:22:13 -0300

    Lista 10 - Métodos

---
+++ exE/exE/exE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exE
+{
+    class exE
+    {
+        static string ListaStrings(List<string> listaStrings)
+        {
+           string resultado = string.Join(" ", listaStrings);
+           
+           return resultado;
+        }
+
+        public static void Main(string[] args)
+        {
+            List<string> listaStrings = new List<string>{"Concatenando", "a", "frase"};
+            
+            string resultado = ListaStrings(listaStrings);
+            
+            Console.WriteLine($"A Lista concatenada: {resultado}");
+        }
+        
+    }
+}
+
