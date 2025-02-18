c03f059c6439b99cf7cfad483e06a87e43118852
Matheus <81193702+Ferning7@users.noreply.github.com>
2025-02-18 11:11:45 -0300

    Lista 11* - Métodos

---
+++ ExJ/ExJ/ExJ.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace ExJ
+{
+    class ExJ
+    {
+        static int MenorNumero(List<int> numeros)
+        {
+            return numeros.Min();
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Insira valores em uma lista (digite 'sair') para sair'");
+            List<int> numeros = new List<int>();
+            
+            while (true)
+            {
+                Console.Write("Digite um numero: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "sair")
+                {
+                    break;
+                }
+                if (int.TryParse(userInput, out int numero))
+                {
+                    numeros.Add(numero);
+                }
+                
+            }
+            Console.WriteLine($"O menor número: {MenorNumero(numeros)}");
+            
+           
+        }
+    }
+}
